88f94b221e6729ce932a913a49bdc706125fd2fb
unknown <josegabriel.jgtc42@gmail.com>
2025-11-12 20:21:23 -0400

    Dark mode background fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,9 +18,9 @@ const Header = () => {
             </div>
 
             {/* Contenido de texto */}
-            <div className="container mx-auto px-0 md:px-8 -m-4 sm:m-0">
-                <div className=" relative z-10 md:absolute md:top-1/2 md:left-12 md:-translate-y-1/2 bg-white p-8 flex flex-col items-center md:items-start text-center md:text-left max-w-lg">
-                    <h2 className="sm:text-[30px] text-[28px] font-bold text-black mb-4">
+            <div className="container mx-auto px-0 md:px-8 -m-6 sm:m-0">
+                <div className=" relative z-10 md:absolute md:top-1/2 md:left-12 md:-translate-y-1/2 bg-white py-8 sm:p-8 flex flex-col items-center md:items-start text-center md:text-left max-w-lg">
+                    <h2 className="text-3xl font-bold text-black mb-4">
                         EQUIPO AGRO DE CALIDAD
                     </h2>
                     <div className='h-0.5 w-20 bg-primary mb-5'></div>
